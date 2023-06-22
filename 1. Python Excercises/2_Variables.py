060f77c78592ab4959f09cbebff54c0b38527b65
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-22 22:54:01 +0600

    Update 2_Variables.py

---
+++ 1. Python Excercises/2_Variables.py
@@ -0,0 +1,17 @@
+# 1. Try to use break as a variable instead of keyword
+break1 = 5
+print(break1)
+
+# 2. Two Variables to determine age
+
+birth_year = 1996
+current_year = 2023
+age = current_year - birth_year
+print("Age:",age, "years") 
+
+# Store first, middle and last name and print full name
+
+first_name = "Rafeed"
+middle_name = "Adnan"
+last_name = "Ibrahim"
+print("My full name is:",first_name,middle_name,last_name)

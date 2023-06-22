de1642f75fc11d66ba20a53abf6db3675e898159
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-22 23:18:55 +0600

    Update 3_Numbers.py

---
+++ 1. Python Excercises/3_Numbers.py
@@ -0,0 +1,26 @@
+# 1. You have a football field that is 92 meter long and 48.8 meter wide. Find out total
+#    area using python and print it
+
+height = 48.8
+width = 92
+
+aread = height * width
+
+print("Area of the football feild is:",aread)
+
+
+
+# 2. You bought 9 packets of potato chips from a store. Each packet costs 1.49 dollar
+#    and you gave shopkeeper 20 dollar.
+#    Find out using python, how many dollars is the shopkeeper going to give you back?
+
+
+
+
+# 3. You want to replace tiles in your bathroom which is exactly square and 5.5 feet
+#    is its length. If tiles cost 500 rs per square feet, how much will be the total
+#    cost to replace all tiles. Calculate and print the cost using python
+#    Hint: Use power operator (**) to find area of a square
+
+
+# 4. Print binary representation of number 17

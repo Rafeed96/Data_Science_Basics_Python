b69a6e7affdfbba47b517d43980c822f24b52c2a
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-22 23:35:14 +0600

    Update 4_Strings.py

--- 1. Python Excercises/4_Strings.py
+++ 1. Python Excercises/4_Strings.py
@@ -14,14 +14,19 @@ print(address)
 #     1. Print "revolves" using slice operator
 #     2. Print "sun" using negative index
 
-
+print("")
+sent = "Earth revolves around the sun"
+print(sent[6:14])
+print(sent[-3:])
 
 # 3. Create two variables to store how many fruits and vegetables you eat in a day.
 # Now Print "I eat x veggies and y fruits daily" where x and y presents vegetables and fruits that you eat everyday.
 # Use python f string for this.
 
-
-
+fruits = 2
+vegetables = 5
+print("")
+print(f"I eat {vegetables} veggies and {fruits} fruits daily")
 
 # 4. I have a string variable called s='maine 200 banana khaye'. This of course is a
 # wrong statement, the correct statement is 'maine 10 samosa khaye'.

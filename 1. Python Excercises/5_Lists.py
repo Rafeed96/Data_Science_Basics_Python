a3a7da366e533af095eeffc8b3aa03bc685abadb
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-23 20:49:26 +0600

    Update 5_Lists.py

--- 1. Python Excercises/5_Lists.py
+++ 1. Python Excercises/5_Lists.py
@@ -9,12 +9,12 @@
 
 expense = [2200, 2350, 2600, 2130, 2190]
 
-# 1. In Feb, how many dollars you spent extra compare to January?
+# 1.1 In Feb, how many dollars you spent extra compare to January?
 
 extra_cost = expense[1] - expense[0]
 print("Extra dollars spent compared to January: ", extra_cost)
 
-# 2. Find out your total expense in first quarter (first three months) of the year.
+# 1.2 Find out your total expense in first quarter (first three months) of the year.
 
 sum = 0
 for i in range(0,3):
@@ -22,7 +22,7 @@ for i in range(0,3):
     
 print("Expenses for the first quarter: ",sum)
 
-# 3. Find out if you spent exactly 2000 dollars in any month
+# 1.3 Find out if you spent exactly 2000 dollars in any month
 
 flag = False
 print(" ")
@@ -35,14 +35,14 @@ if flag == True:
 else:
     print("No")
 
-# 4. June month just finished and your expense is 1980 dollar. Add this item to our monthly expense list
+# 1.4 June month just finished and your expense is 1980 dollar. Add this item to our monthly expense list
 print("")
 June = 1980
 expense.append(June)
 print(expense)
 
 
-# 5. You returned an item that you bought in a month of April and
+# 1.5 You returned an item that you bought in a month of April and
 # got a refund of 200$. Make a correction to your monthly expense list
 # based on this
 print("")
@@ -50,3 +50,8 @@ refund = 200
 
 expense[3] = expense[3]-refund
 print("Updated Expense after refund: ",expense)
+
+
+# 2. You have a list of your favourite marvel super heros
+# heros=['spider man','thor','hulk','iron man','captain america']
+# Using this list

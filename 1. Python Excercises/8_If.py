635e375690aa7392bc4ac31a3d057d9a16126bc0
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-23 21:52:03 +0600

    Update 8_If.py

--- 1. Python Excercises/8_If.py
+++ 1. Python Excercises/8_If.py
@@ -1,5 +1,5 @@
 ## Exercise: Python If Condition
-# 1. Using following list of cities per country,
+# 1.1 Using following list of cities per country,
 #     ```
 #     india = ["mumbai", "banglore", "chennai", "delhi"]
 #     pakistan = ["lahore","karachi","islamabad"]
@@ -22,3 +22,27 @@ elif city in bangladesh:
     print("You are in Bangladesh")
 else:
     print("You are in planet Earth")
+
+print("")
+
+## Exercise: Python If Condition
+# 1.2 Using following list of cities per country,
+#     ```
+#     india = ["mumbai", "banglore", "chennai", "delhi"]
+#     pakistan = ["lahore","karachi","islamabad"]
+#     bangladesh = ["dhaka", "khulna", "rangpur"]
+#     ```
+#     Write a program that asks user to enter two cities and it tells you if they both are in same country or not. For example if I enter mumbai and chennai, it will print "Both cities are in India" but if I enter mumbai and dhaka it should print "They don't belong to same country"
+
+
+india = ["mumbai", "banglore", "chennai", "delhi"]
+pakistan = ["lahore","karachi","islamabad"]
+bangladesh = ["dhaka", "khulna", "rangpur"]
+
+city1 = "dhaka"
+city2 = "rangpur"
+
+if city1 in bangladesh and city2 in bangladesh or city1 in india and city2 in india or city1 in pakistan and city2 in pakistan :
+    print("You are in the same country")
+else:
+    print("You are not in the same country")
